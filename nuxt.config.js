c5981ab4161e2591a91b60d3c413e03d3175272c
Mohamed Nashaat <mohamed.nashaat@mondia.com>
2021-05-07 13:12:39 +0200

    adding config to heroku

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
-  target: 'static' ,
+  target: process.env.TARGET ||  'static' ,
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - hijri',
