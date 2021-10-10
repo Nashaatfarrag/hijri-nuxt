354690c685ed854ce5d43fb3fc8e16e7202cfa33
romell491 <ulgasan491@yahoo.com>
2021-10-10 23:54:45 +0800

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,9 +25,6 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-
-    { src: '~/plugins/vue-gtag', },
-
     { src: '~/plugins/clipboard.js' }
   ],
 
